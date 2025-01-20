8a47bb41c30456856ac43719f63b17144208ffd7
Bart Massey <bart.massey@gmail.com>
2025-01-19 16:07:33 -0800

    card probability calculation lib

---
+++ flopset.py
@@ -0,0 +1,34 @@
+from pycard import *
+from math import comb
+
+# Probability of flopping a set. A "set" here includes quads: it
+# would not be too hard to change this, but I think folks hoping
+# for a set will be perfectly happy with quads.
+
+# https://upswingpoker.com/odds-flopping-each-poker-hand/
+p_known = 0.118
+
+# Calculated probability
+
+# This is the number of possible draws
+# once you are holding a given pocket pair.
+ndraws = comb(50, 3)
+
+# This is the number of ways you can miss your set
+# by hitting three other cards.
+nmisses = comb(48, 3)
+
+# The probability is 1 - unfavorable outcomes / total outcomes
+p_calced = 1.0 - nmisses / ndraws
+
+# Sampled probability. Without loss of generality,
+# we will hold pocket aces.
+p_sampled = sample(
+    100_000,
+    3,
+    lambda draw: any(rank(c) == 1 for c in draw),
+    deck=fresh_deck()[2:],
+)
+
+# These should agree to at least 2 decimal places.
+print(p_known, p_calced, p_sampled)
---
+++ pycard.py
@@ -0,0 +1,39 @@
+import random, unittest
+from collections.abc import Callable
+from typing import List
+
+# Returns cards in rank, suit order. Rank order
+# is A, 2, 3, .. Q, K.
+def fresh_deck() -> List[int]:
+    return [c for c in range(52)]
+
+# Rank is 1 for A, 11 for J, 12 for Q, 13 for K
+def rank(c: int) -> int:
+    return c // 13 + 1
+
+# Suit is 0 for Clubs, 1 for Diamonds, 2 for Hearts, 3 for Spades.
+def suit(c: int) -> int:
+    return c % 4
+
+def sample(
+    ntrials: int,
+    ncards: int,
+    check: Callable[[int], bool],
+    deck: List[int] = fresh_deck(),
+):
+    matches = 0
+    for _ in range(ntrials):
+        drawn = random.sample(deck, ncards)
+        matches += bool(check(drawn))
+    return matches / ntrials
+
+# https://www.dataquest.io/blog/unit-tests-python/
+class TestCalculations(unittest.TestCase):
+
+    # XXX This test will accidentally fail with very low probability.
+    def test_sample(self):
+        p = sample(1000, 1, lambda d: rank(d[0]) == 0)
+        self.assertTrue(abs(p - 1/13) < 0.1)
+
+if __name__ == '__main__':
+    unittest.main()
